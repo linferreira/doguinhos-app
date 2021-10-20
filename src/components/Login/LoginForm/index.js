725b173fb14d5e2ea8c7f9b8f3d5500102c4b2ac
Lindsay Ferreira <lindsayferreira@ioasys.com.br>
2021-10-20 12:48:39 -0300

    feat: create sub components Login

---
+++ src/components/Login/LoginForm/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      }).then((response) => {
+        console.log(response);
+
+        return response.json();
+      }).then((json) => {
+        console.log(json);
+      });
+    }
+
+    return <section>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='text' 
+                value={username} 
+                onChange={({target}) => setUserName(target.value)} 
+            />
+
+            <input 
+                type='password' 
+                value={password} 
+                onChange={({target}) => setPassword(target.value)} 
+            />
+
+            <button>Enviar</button>
+        
+        </form>
+        <Link to='/login/criar'>Cadastro</Link>
+    </section>
+}
+
+export default LoginForm
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
-    return <div>Login</div>
+    return <div>
+        <Routes>
+            <Route path='/' element={<LoginForm />}></Route>
+            <Route path='criar' element={<LoginCreate />}></Route>
+            <Route path='perdeu' element={<LoginPasswordLost />}></Route>
+            <Route path='resetar' element={<LoginPasswordReset />}></Route>
+        </Routes>
+    </div>
 }
 
 
