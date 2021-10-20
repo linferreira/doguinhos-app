b6161625455b24cc20f92d6a872fb744a15e40fe
Lindsay Ferreira <lindsayferreira@ioasys.com.br>
2021-10-20 16:31:37 -0300

    feat: create useForm hook

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+};
+
+const useForm = (formType) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(null);
+
+  function onChange({ target }) {
+    if (error) {
+      validate(target.value);
+    }
+    setValue(target.value);
+  }
+
+  function validate(value) {
+    if (!formType) {
+      return true;
+    }
+    if (value.length === 0) {
+      setError("Preencha um valor!!");
+      return false;
+    } else if (types[formType] && !types[formType].regex.test(value)) {
+      setError(types[formType].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
